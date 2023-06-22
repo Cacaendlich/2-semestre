50696211f9a48faf90a6297e55d825357d2d531a
caca endlich <camilaendlichpimenta.trab@gmail.com>
2023-06-22 20:05:04 -0300

    upload das funções de validação

--- AVA2/app.js
+++ AVA2/app.js
@@ -1,19 +1,19 @@
     function validarNome() {
         const nome = document.querySelector('input[name="tName"]').value;
-        
-        const isValid = String(nome).length >= 15;
-
-        if (!isValid) {
-            alert("Parece que o seu nome está incompleto.");
-        }
-
-        return isValid;
+        const regex = /[A-Za-z ]{15}[A-Za-z ]*/g;
+        validarRegex(nome, regex);
     }
-    
+
+    function validarRegex(value, regex) {
+        if (!regex.test(value)) {
+            alert("O nome está incompleto.");
+        }
+    }
+
     function validarEmail() {
         const email = document.querySelector('input[name="tEmail"]').value;
         
-        const isValid = email.includes("@") > 0 && email.includes(".") >= 1 && String(email).length > 10;
+        const isValid = email.indexO("@") > 0 && email.includes(".") >= 1 && String(email).length > 10;
 
         if (!isValid) {
             alert("Opa! Parece que o email inserido é inválido. Certifique-se de que o email contenha o símbolo '@', um domínio válido (como exemplo.com) e tenha pelo menos 10 caracteres.");
@@ -38,26 +38,29 @@
     function campoSelecionado() {
         const estadoCivil = document.getElementById('cEstadoCivil').value;
         const dataNascimento = document.getElementById('cdataNascimento').value;
-        // const idade = caucularIdade(dataNascimento);
-        const idade = 16;
+        const idade = calcularIdade(dataNascimento);
 
-        const isValid = estadoCivil === "solteiro" && idade > 15;
-
-        if (!isValid) {
-            alert('Por favor, verifique se você possui mais de 16 anos antes de prosseguir com o cadastro.');
+        if (idade > 15 ) {
+            return true;
+        } else if(idade < 15 && estadoCivil !== "solteiro" ) {
+            alert('Por favor, verifique se você possui 15 anos ou mais antes de prosseguir com o cadastro.');
+            return false;
         }
+        
 
-        return isValid;
     }
 
     function calcularIdade(anoNascimento){
-        const idade = new Date().getFullYear() - anoNascimento.getFullYear();
+        let dataNascimento = new Date(anoNascimento);
+        let dataHoje = new Date();
+        const diffTime = Math.abs(dataHoje - dataNascimento);
+        const idade = Math.floor(diffTime / (1000 * 60 * 60 * 24 * 365));
         return idade;
     }
     
     function validar(e) {
         e.preventDefault(); // Linha utilizada para não recarregar a página quando o form recebe o submit
         if (validarNome() && validarEmail() && validarData() && campoSelecionado()) {
-            alert(' Seu cadastro foi concluído com sucesso!');
+            alert('Dados enviados');
         }
     }
