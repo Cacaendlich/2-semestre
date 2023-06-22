4c0d039273f82a84066de12dfc6968168ebeec52
Felipe Endlich <endlichfelipe@gmail.com>
2023-06-22 16:24:05 -0300

    good practices

--- AVA2/app.js
+++ AVA2/app.js
@@ -1,63 +1,63 @@
     function validarNome() {
-        var nome = document.querySelector('input[name="tName"]').value;
-    
-        if (String(nome).length >= 15) {
-            return true;
-        } else {
+        const nome = document.querySelector('input[name="tName"]').value;
+        
+        const isValid = String(nome).length >= 15;
+
+        if (!isValid) {
             alert("Parece que o seu nome está incompleto.");
-            return false;
         }
+
+        return isValid;
     }
     
     function validarEmail() {
-        var email = document.querySelector('input[name="tEmail"]').value;
-    
-        if (email.includes("@") > 0 && email.includes(".") >= 1 && String(email).length > 10) {
-            return true;
-        } else {
+        const email = document.querySelector('input[name="tEmail"]').value;
+        
+        const isValid = email.includes("@") > 0 && email.includes(".") >= 1 && String(email).length > 10;
+
+        if (!isValid) {
             alert("Opa! Parece que o email inserido é inválido. Certifique-se de que o email contenha o símbolo '@', um domínio válido (como exemplo.com) e tenha pelo menos 10 caracteres.");
-            return false;
         }
+        
+        return isValid;
     }
 
     function validarData(){
-        var inputData = document.getElementById('cdataNascimento').value;
+        const inputData = document.getElementById('cdataNascimento').value;
         const dataNascimento = new Date(inputData);
-
-        if(isNaN(dataNascimento)){
+        
+        const isValid = !isNaN(dataNascimento);
+        
+        if(!isValid){
             alert('Parece que a data de nascimento inserida é inválida. Por favor, verifique e tente novamente.');
-            return false;
         }  
-        return true;
+
+        return isValid;
     }
 
     function campoSelecionado() {
-        var estadoCivil = document.getElementById('cEstadoCivil').value;
-        var dataNascimento = document.getElementById('cdataNascimento').value;
-        var anoNascimento = getYear(dataNascimento);
-        var idade = caucularIdade(anoNascimento);
-        
-        if(estadoCivil === "1" && idade > 16){
-            return true;
-        } else {
+        const estadoCivil = document.getElementById('cEstadoCivil').value;
+        const dataNascimento = document.getElementById('cdataNascimento').value;
+        // const idade = caucularIdade(dataNascimento);
+        const idade = 16;
+
+        const isValid = estadoCivil === "solteiro" && idade > 15;
+
+        if (!isValid) {
             alert('Por favor, verifique se você possui mais de 16 anos antes de prosseguir com o cadastro.');
-            return false;
         }
+
+        return isValid;
     }
 
-    function caucularIdade(anoNascimento){
-        var anoAtual = new Date();
-        idade = anoAtual.getFullYear() - anoNascimento.getFullYear();
+    function calcularIdade(anoNascimento){
+        const idade = new Date().getFullYear() - anoNascimento.getFullYear();
         return idade;
     }
     
-    function validar() {
-        var nomeValido = validarNome();
-        var emailValido = validarEmail();
-        var dataValida = validarData();
-
-        if (nomeValido && emailValido && dataValida && campoSelecionado()) {
+    function validar(e) {
+        e.preventDefault(); // Linha utilizada para não recarregar a página quando o form recebe o submit
+        if (validarNome() && validarEmail() && validarData() && campoSelecionado()) {
             alert(' Seu cadastro foi concluído com sucesso!');
         }
     }
-    
